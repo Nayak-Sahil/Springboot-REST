0163fe620a1612ef53ddde19a460f13484af2cd3
Sahil Nayak <sahil.n@incubyte.co>
2025-03-26 16:01:07 +0530

    Add(Dependency): ModelMapper to Convert DTO into Entity (vice versa)

--- src/main/java/com/learning/springboot/api/configuration/AppConfig.java
+++ src/main/java/com/learning/springboot/api/configuration/AppConfig.java
@@ -3,6 +3,7 @@ package com.learning.springboot.api.configuration;
 import com.learning.springboot.api.database.Database;
 import com.learning.springboot.api.database.Local;
 import com.learning.springboot.api.database.Production;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,4 +18,7 @@ public class AppConfig {
     @Bean
     @ConditionalOnProperty(name = "spring.app.mode", havingValue = "production")
     public Database getProductionDatabase() { return new Production(); }
+
+    @Bean
+    public ModelMapper getModelMapper() { return new ModelMapper(); }
 }
