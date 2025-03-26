3871b9e772e8e66492b0d729676c3cd377ea3d7f
Sahil Nayak <sahil.n@incubyte.co>
2025-03-26 15:59:16 +0530

    Fix(Repository): Delete older local list-data based repository

--- src/main/java/com/learning/springboot/api/repositories/ObjectiveRepository.java
+++ src/main/java/com/learning/springboot/api/repositories/ObjectiveRepository.java
@@ -1,33 +1,10 @@
 package com.learning.springboot.api.repositories;
 
-import com.learning.springboot.api.dto.ObjectivesDTO;
+import com.learning.springboot.api.entities.ObjectivesEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 @Repository
-public class ObjectiveRepository {
-    List<ObjectivesDTO> database = new ArrayList<ObjectivesDTO>();
-
-    public List<ObjectivesDTO> get() {
-        return database;
-    }
-
-    public ObjectivesDTO get(String id) {
-        return database.stream().filter(obj -> obj.getId().equals(id)).findFirst().orElse(null);
-    }
-
-    public ObjectivesDTO insert(ObjectivesDTO data) {
-        database.add(data);
-        return data;
-    }
-
-    public ObjectivesDTO delete(String id) {
-        ObjectivesDTO objectiveToBeDeleted = database.stream().filter(obj -> obj.getId().equals(id)).findFirst().orElse(null);
-        database = database.stream().filter(obj -> !obj.getId().equals(id)).collect(Collectors.toList());
-        return objectiveToBeDeleted;
-    }
+// This interface provides CRUD operations for the "ObjectivesEntity" class with a String type primary key.
+public interface ObjectiveRepository extends JpaRepository<ObjectivesEntity, String> {
 }
--- src/main/java/com/learning/springboot/api/repositories/ObjetiveRepository.java
+++
@@ -1,8 +0,0 @@
-package com.learning.springboot.api.repositories;
-
-import com.learning.springboot.api.entities.ObjectivesEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// This interface provides CRUD operations for the "ObjectivesEntity" class with a String type primary key.
-public interface ObjetiveRepository extends JpaRepository<ObjectivesEntity, String> {
-}
